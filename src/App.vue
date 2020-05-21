fb4ab5f13e415a69516b1cfba43951133c122e97
sa <psa5013@hanmail.com>
2020-05-21 17:33:05 +0900

    사전 data 비동기(axios) 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <news-view></news-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewsView from './components/NewsView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewsView,
   }
 }
 </script>
---
+++ src/components/NewsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      {{ datas }}
+  </div>
+</template>
+
+<script>
+import { fetchProdsList } from '../api/index.js';
+
+export default {
+    data() {
+        return {
+            datas: [],
+        }
+    },
+    created() {
+        fetchProdsList()
+            .then( res => {
+                this.datas = res.data.data;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    },
+
+}
+</script>
+
+<style>
+
+</style>
